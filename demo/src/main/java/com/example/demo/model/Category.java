230728bbaf2033ae33a216ae95a8641ba7f76c8a
Keenal <keenal.shah@ufl.edu>
2020-11-24 14:37:06 -0500

    finish the backend part with bootstrap

--- demo/src/main/java/com/example/demo/model/Category.java
+++ demo/src/main/java/com/example/demo/model/Category.java
@@ -1,24 +1,26 @@
 package com.example.demo.model;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 @Entity
+@NoArgsConstructor
+@Data
 @Table(name="category")
 public class Category {
-	
 	@Id
 	private Long id;
-	
+
 	private String name;
-	
-	@ManyToOne(cascade=CascadeType.PERSIST)
-	private User user; 
 	
 	
 
