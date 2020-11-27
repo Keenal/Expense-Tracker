eb40f756933aa15e27b5344faa7ec94f99e42c37
Keenal <keenal.shah@ufl.edu>
2020-11-26 20:01:30 -0500

    add home react

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './Category'
+import 'bootstrap/dist/css/bootstrap.css'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Home />,
   document.getElementById('root')
 );
 
